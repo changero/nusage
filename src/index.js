25b931d61567cb8ae4b29b092024192d12f715d1
changero <changero@126.com>
2022-08-21 19:06:12 +0800

    feat: 添加等待状态

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import { resolve } from "path";
 import { readdir, lstat } from "fs/promises";
 import chalk from "chalk";
+import ora from "ora";
+
 import { formatSize, formatPercent, formatTime } from "./utils.js";
 
+const spinner = ora("正在计算...");
 const root = process.cwd();
 
 (async function () {
+  spinner.start();
   const start = Date.now();
 
-  const { total, map } = await getDirSize(root);
+  const { total, map } = await getDirSize(root, (file) => {
+    spinner.text = `正在计算...${file}`;
+  });
+  spinner.stop();
   console.log(`Total: ${formatSize(total)}    ${chalk.green(root)}`);
   console.log("-".repeat(60));
   const sorted = [...Object.entries(map)].sort(
@@ -33,20 +40,21 @@ const root = process.cwd();
     }
   }
 
-  console.log(`耗时: ${formatTime(Date.now() - start)}`);
+  spinner.succeed(`操作完成，耗时: ${formatTime(Date.now() - start)}`);
 })();
 
-async function getDirSize(directory) {
+async function getDirSize(directory, cb) {
   const files = await readdir(directory);
 
   const map = {};
 
   const total = await files.reduce(async (total, file) => {
+    if (cb && typeof cb === "function") cb(file);
     let t = await total;
     const filePath = resolve(directory, file);
     const filestat = await lstat(filePath);
     if (filestat.isDirectory()) {
-      const { total: dirSize } = await getDirSize(filePath);
+      const { total: dirSize } = await getDirSize(filePath, cb);
       t += dirSize;
       map[file] = {
         type: 1,
